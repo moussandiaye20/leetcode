81ddcfc6f66a2b16158346b87f05b0ab6a42538b
moussandiaye20 <ndiayemoussa1960@gmail.com>
2022-03-13 21:40:29 +0100

    283

--- 283/283.py
+++ 283/283.py
@@ -15,3 +15,19 @@ class Solution:
             noZero+=1
         
         return nums
+        
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left=right=0
+        
+        while left<len(nums):
+            if nums[left]!=0:
+                nums[left],nums[right]=nums[right],nums[left]
+                right+=1
+            left+=1
+        return nums
