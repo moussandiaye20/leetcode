42c9a9978b52277a78a7dbd81ff5ce9805d8532c
moussandiaye20 <ndiayemoussa1960@gmail.com>
2022-03-17 19:49:25 +0100

    88

--- 88/88.py
+++ 88/88.py
@@ -11,11 +11,11 @@ class Solution:
             if nums1[m]>nums2[n]:
                 nums1[full]=nums1[m]
                 m-=1
-                full-=1
+              
             else:
                 nums1[full]=nums2[n]
                 n-=1
-                full-=1
+            full-=1
       
         while n>=0:
             nums1[full]=nums2[n]
