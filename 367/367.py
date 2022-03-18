5d14a3406a4d9a7420c53a56b9c44fa711a14b8b
moussandiaye20 <ndiayemoussa1960@gmail.com>
2022-03-18 22:00:37 +0100

    367

---
+++ 367/367.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        
+        left,right=0,num
+        while left<=right:
+            
+            mid=left+(right-left)//2
+            
+            if mid*mid==num:return True
+            elif mid*mid>num:
+                right=mid-1
+            else:
+                left=mid+1
+                
+        
+        
+        return False
+        
